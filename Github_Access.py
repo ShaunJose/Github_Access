afc58319260036c44bd2d83f58afc1525c9cb125
ShaunJose <shaun/jose12@gmail.com>
2018-11-21 22:22:46 +0000

    Threw away initial pulling code. Starting fresh as I'm getting different info.

--- Github_Access.py
+++ Github_Access.py
@@ -1,55 +1,14 @@
 from github import Github
 import sys
 
-# declaring my access token
+# declaring my access token --> (given no permissions)
 access_token = "1f24b75e991ab17fa02a3bb1df8dec255e77fbd6"
 
 # creating github instance using token
 g = Github(access_token)
 
-#getting my profile
-me = g.get_user()
+#getting organisation 'github'
+github = g.get_organization("github")
 
-print("-----------------------------") #delimiter
-
-#printing stuff about my profile
-print("User's Name: " + me.name)
-print("About the user: " + me.bio)
-
-print("-----------------------------") #delimiter
-
-#getting someone else's profile
-god = g.get_user("god")
-
-#printing stuff about user "god"
-name = god.name
-
-print("Information about " + name)
-
-#printing out Location
-print("Location: " + god.location)
-
-#fetching all of God's Repositories and info regarding them, and displaying it
-repos = god.get_repos()
-ctr = 1;
-print("Repositories, Forks and Issues: ")
-for repo in repos:
-    sys.stdout.write(str(ctr) + ". " + repo.name)
-    #Forks from current repo
-    forks_count = repo.forks_count
-    if(forks_count == 0):
-        sys.stdout.write(" - No forks from this repo. ")
-    else:
-        sys.stdout.write(" - " + str(forks_count) + " fork/s from this repo. ")
-    sys.stdout.flush()
-    #Issues in current repo
-    issues_count = repo.open_issues_count
-    if(issues_count == 0):
-        print("No issues in this repo")
-    else:
-        print(str(issues_count) + " issue/s in this repo")
-    #increment counter
-    ctr = ctr + 1
-
-#if god.has_issues():
-#    issue_urls = god.issue_urls()
+#getting members of github as a list of 'NamedUser' objects, with their logins
+member_objects = list(github.get_members())
--- README.md
+++ README.md
@@ -2,9 +2,26 @@
 
 Using the Github API (in Python) to retrieve and display data regarding one or more developers.
 
-*Tools used:
+
+# Tools used:
 1. PyGithub.
    Install: pip install PyGithub
 
-2. Pytest
+2. Pytest.
    Install: pip install -U pytest*
+
+
+# Data Gathering (Plan for now):
+All the member's who work in github, and recorded on github.com/github, are gathered as github users.
+
+Their location, as well as repositories are gathered as well.
+
+
+# Visualization (Plan for now):
+1. Nodes are members of github.
+
+2. Color-coded nodes, based on location.
+
+3. Links between nodes who have worked on the same repositories
+
+4. Clicking on the node display's user's information
